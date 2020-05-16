004f562dc99e36a60d874715b2d75617cc868e67
Robert Leduc <rleduc34@gmail.com>
2020-05-16 18:51:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,8 @@
 def reverse_each_word(string)
-  string.collect do |new_string|
-    string.reverse
+  array = string.split (" ")
+  new_array = []
+  array.collect do |string|
+    new_array << string.reverse
+  end
+  new_array.join(" ")
 end
